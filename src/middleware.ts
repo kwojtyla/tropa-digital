58f718fcab48e5aef3a21263866fc742b42b1ba1
Karol Wojtyla <karolwojtyla360@gmail.com>
2025-06-07 15:56:37 -0300

    chore: configure middleware

---
+++ src/middleware.ts
@@ -0,0 +1,27 @@
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+import { RequestCookie } from "next/dist/compiled/@edge-runtime/cookies";
+import { cookies } from "next/headers";
+
+const publicRoutes = ["/login"];
+
+export async function middleware(req: NextRequest) {
+  const { pathname } = req.nextUrl;
+  const cookieStore = await cookies();
+
+  if (publicRoutes.includes(pathname)) {
+    return NextResponse.next();
+  }
+
+  const sessionCookie: RequestCookie | undefined = cookieStore.get("jwt");
+
+  if (!sessionCookie) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+}
+
+export const config = {
+  matcher: [
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$|api|healthcheck|manifest.json).*)",
+  ],
+};
