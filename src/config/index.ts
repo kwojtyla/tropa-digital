6dad4f4f06ce254cd5c47e3802faa9f9e5e97ef2
Karol Wojtyla <karolwojtyla360@gmail.com>
2025-06-08 12:06:39 -0300

    feat: add providers and config

---
+++ src/config/index.ts
@@ -0,0 +1,11 @@
+const createSiteConfig = () => {
+  return {
+    name: "Agência Tropa Digital",
+    url: "https://tropa.digital",
+    description:
+      "Criamos sites e experiências digitais que ajudam sua empresa a a se destacar no mundo online",
+    ogImage: "https://tropa.digital/og.jpg",
+  };
+};
+
+export const siteConfig = createSiteConfig();
